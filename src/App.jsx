98c1ecb010143bda0acfd1db968bdfaefb955fdc
Noman Mia <nomanislamnishad@gmail.com>
2024-03-10 17:30:22 +0600

    explore usestate useeffect

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Posts from './Posts'
+import './App.css'
+
+function App() {
+
+
+  return (
+
+    <>
+      <h1>Vite + React</h1>
+      <Posts></Posts>
+    </>
+  )
+}
+
+export default App
---
+++ src/Post.jsx
@@ -0,0 +1,10 @@
+export default function Post({post}){
+ const {id,title,body} = post;
+    return(
+        <div style={{border:"2px solid red", padding:"20px", margin:"20px", borderRadius:"10px"}}>
+            <h3>Title :{title} </h3>
+            <p>Id : {id}</p>
+            <p>Body : {body}</p>
+        </div>
+    )
+}
